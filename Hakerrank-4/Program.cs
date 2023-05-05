cb318b6eb3d73a49e6f61ebcf8521774209d9f6d
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-05-05 14:46:47 +0300

    Add files via upload

---
+++ Hakerrank-4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Person {
+    public int age;     
+	public Person(int initialAge) {
+        
+     }
+     public void amIOld() {
+    
+     }
+
+     public void yearPasses() {
+    
+     }
+
+static void Main(String[] args) {
+        int T=int.Parse(Console.In.ReadLine());
+        for (int i = 0; i < T; i++) {
+            int age=int.Parse(Console.In.ReadLine());
+            Person p=new Person(age);
+             p.amIOld();
+                for (int j = 0; j < 3; j++) {
+                  p.yearPasses();
+                }
+                p.amIOld();
+                Console.WriteLine();
+        }
+    }
+}
