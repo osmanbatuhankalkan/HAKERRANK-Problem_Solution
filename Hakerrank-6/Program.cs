20dd39785e3a9a39d73323d96cd081ba3b1cd489
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-05-11 22:39:04 +0300

    Add files via upload

---
+++ Hakerrank-6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace HakerRank_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //kaç tane string deger alacağımızı yazalım
+            System.Console.WriteLine("Değer Girin");
+            int n = int.Parse(Console.ReadLine());
+            for(int i=0; i<10; i++ )
+            {   
+                for(int a=0; a<n.Length; a++)
+                {
+                    if(n%2==0)
+                    {
+                        System.Console.WriteLine(n);
+                    }
+                    else
+                        System.Console.WriteLine(n);
+                }
+                System.Console.WriteLine(n);
+                //Console.ReadKey();
+            }
+        }
+    }
+}
