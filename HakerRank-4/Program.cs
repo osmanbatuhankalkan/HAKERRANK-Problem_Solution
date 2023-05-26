c5a58ca63ce0d5bc7ae5dc31f0677c24826963de
Osman Batuhan Kalkan <119897554+osmanbatuhankalkan@users.noreply.github.com>
2023-05-26 23:30:57 +0300

    Add files via upload

---
+++ HakerRank-4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Person
+{
+class program
+        {
+    static void Main(String[] args) {
+        System.Console.WriteLine("a");
+        int age=int.Parse(Console.ReadLine());
+            
+        if(age<=0)
+        {
+            System.Console.WriteLine("Age is not valid, setting age to 0.");
+        } 
+        else if(age<13)
+        {
+            System.Console.WriteLine("You are young");
+        }
+        else if(age>=13 && age<18)
+        {
+            System.Console.WriteLine("you are a teenager");
+            
+        } 
+        else
+            System.Console.WriteLine("You are old");
+            
+        }
+
+    }
+}
